e2bbe4a30b95f1670ffcda088acf26588d0305c7
kevicebryan <kevinbryanreligion@gmail.com>
2024-08-04 19:02:37 +0700

    ep3: show datas to the view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import supabase from "../config/supabaseClient";
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, err } = await supabase.from("smoothies").select("");
+      if (err) {
+        setFetchError("Could not fetch smoothies");
+        setSmoothies(null);
+        console.log(err);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError.message}</p>}
+      {smoothies &&
+        smoothies.map((smoothie) => {
+          return (
+            <div className="smoothies">
+              <div className="smoothie-grid">
+                {/* order-by buttons */}
+                <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
